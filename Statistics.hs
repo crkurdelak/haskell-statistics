744a46ba4fdf84e8475fb4b0a1da96bd3b89f684
crkur <ckurdelak20@georgefox.edu>
2023-10-17 14:57:23 -0700

    fixed stuff yay

--- Statistics.hs
+++ Statistics.hs
@@ -10,20 +10,20 @@ main = do
     -- get file contents
     contents <- readFile "grades.dat"
     -- get floats out of contents
-    let floats = readMaybe <$> lines contents :: [Maybe Float]
+    let floats = read <$> lines contents :: [Float]
 
     --do calculations and show results
 
-    let results = [show x | x <- calcStats floats] :: [String]
+    --let results = [show x | x <- calcStats floats] :: [String]
 
     --putStrLn "Mean: " : results[0] : "\nMin: " : results[1] : "\nMax: " : results[2] : "\nStdev: " : results[3] : []
     --let resultstring = "Mean: " : show <$> calcMean floats : "\nMin: " : show <$> minimum floats : "\nMax: " : show <$> maximum floats : "\nStdev: " : show <$> calcStdev floats : []
     --putStrLn resultstring
 
-    putStrLn <$> show <$> calcMean floats
-    putStrLn <$> show <$> minimum floats
-    putStrLn <$> show <$> maximum floats
-    putStrLn <$> show <$> calcStdev floats
+    putStrLn (show (calcMean floats))
+    putStrLn (show (minimum floats))
+    putStrLn (show (maximum floats))
+    putStrLn (show (calcStdev floats))
 
     --putStrLn show results
 
@@ -32,20 +32,20 @@ main = do
     --map putStrLn (map show (calcStats floats))
 
 
-calcStats :: [Maybe Float] -> [Maybe Float]
+calcStats :: [Float] -> [Float]
 calcStats floatlist =
     calcMean floatlist : minimum floatlist : maximum floatlist : calcStdev floatlist : []
 
 
-calcMean :: [Maybe Float] -> Maybe Float
-calcMean floatlist = sum (floatlist / (fromIntegral length floatlist))
+calcMean :: [Float] -> Float
+calcMean floatlist = (sum floatlist) / (fromIntegral (length floatlist))
 
 
-calcStdev :: [Maybe Float] -> Maybe Float
+calcStdev :: [Float] -> Float
 --calcStdev floatlist = map sqrt (((sum x - calcMean floatlist)^2) / length floatlist) floatlist
 -- currently it's trying to subtract scalar from list
 --let meandiffs = [x - calcmean floatlist | x <- floatlist]
 --calcStdev floatlist = sqrt ((sum (floatlist - calcMean floatlist))**2)
 
 --uses a list comprehension wow so haskell
-calcStdev floatlist = sqrt ((sum [x - calcMean floatlist | x <- floatlist])**2)
+calcStdev floatlist = sqrt (sum [(x - (calcMean floatlist))**2 | x <- floatlist])
